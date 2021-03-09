3df7050ef395b5a8be48912168ccfb0bdb662306
bautd-2046 <tran.duy.bau@sun-asterisk.com>
2021-03-09 20:48:39 +0700

    refs lesson: dynamic routing

--- index.js
+++ index.js
@@ -14,6 +14,11 @@ app.get('/detail', (request, response) => {
     response.send('Chi tiết bài viết')
 })
 
+// dynamic routing
+app.get('/categories/:category_id', (request, response) => {
+    console.log(request.params.category_id, '--- request.params.category_id'); // eslint-disable-line no-console
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })

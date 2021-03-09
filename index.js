caa2925172da6d2c729a9c68cc40adbdd8996780
bautd-2046 <tran.duy.bau@sun-asterisk.com>
2021-03-09 21:17:36 +0700

    refs lesson: Moving around by routing

--- index.js
+++ index.js
@@ -1,24 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3333;
 
 app.get('/', (request, response) => {
-    response.send('Trang chủ')
-})
+    response.send(`
+        <p>
+            <a href="/about-us">About Us</a>
+        </p>
+        <p>
+            <a href="/random-route/${randomKey()}">Go to random route</a>
+        </p>
+    `);
+});
 
 app.get('/about-us', (request, response) => {
-    response.send('Thông tin về chúng tôi')
-})
+    response.send('<a href="/">Back to homepage</a>');
+});
 
-app.get('/detail', (request, response) => {
-    response.send('Chi tiết bài viết')
-})
-
-// dynamic routing
-app.get('/categories/:category_id', (request, response) => {
-    console.log(request.params.category_id, '--- request.params.category_id'); // eslint-disable-line no-console
+app.get('/random-route/:random_key', (request, response) => {
+    const randomId = randomKey()
+    response.send(`
+        <p><a href="/random-route/${randomId}">Go to route: ${randomId}</a></p>
+        <p><a href="/">Go back to homepage</a></p>
+    `)
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`this app is running in port ${port}`); // eslint-disable-line no-console
+});
+
+const randomKey = () => Math.random().toString(32).slice(2)
